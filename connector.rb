fb72ded3a8f44b4045583c3c836717171c44341e
Rodrigo <rodrigo@datamob.net.br>
2021-01-23 09:29:55 -0300

    License
    Adding license

--- connector.rb
+++ connector.rb
@@ -1,3 +1,18 @@
+#  Bind Manager - DNS control system using BIND9, where it is possible to create, change and delete through a REST API
+#  Copyright (C) 2021  Rodrigo Mello Nardi
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 require 'active_record'
 require 'active_support'
 require 'logger'
@@ -10,7 +25,7 @@ class Connector
   include Singleton
 
   def initialize
-    @config                   = YAML::load(IO.read("#{File.dirname(__FILE__)}/database/config.yml"))
+    @config                   = YAML::load(IO.read("#{File.dirname(__FILE__)}/db/config.yml"))
     @logger                   = Logger.new(STDOUT)
     @logger.level             = Logger::WARN
     ActiveRecord::Base.logger = @logger
--- db/models/domain.rb
+++ db/models/domain.rb
@@ -1,8 +1,23 @@
+#  Bind Manager - DNS control system using BIND9, where it is possible to create, change and delete through a REST API
+#  Copyright (C) 2021  Rodrigo Mello Nardi
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU Affero General Public License as published
+#  by the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU Affero General Public License for more details.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 class Domain < ActiveRecord::Base
-  validates :zone, presence: true, length: {maximum: 512}
+  validates :zone, uniqueness: true, presence: true, length: {maximum: 512}
   validates :mode, presence: true, length: {maximum: 512}
   validates :file, presence: true, length: {maximum: 512}
-  validates :allow_transfer, presence: true, length: {maximum: 512}
+  validates :allow_transfer, uniqueness: true, presence: true, length: {maximum: 512}
 
   def to_bind9
     "
